fe18e094ed8c22752582e4253113691251fcfa7f
Saransh Gupta <82395600+saranshusa@users.noreply.github.com>
2021-10-14 14:13:12 +0530

    Quizie App added

---
+++ quie/src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li>Play</li>
+                <li>Login</li>
+            </ul>
+        </div>
+    )
+}
+
+export default navbar
